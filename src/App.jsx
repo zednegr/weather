afa397aeb4fbcc9a3ee821536705c98139d9807d
zednegr <negrzed@gmail.com>
2023-08-09 10:55:32 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Weather from './components/weather/weather'
 
 function App() {
 
   return (
     <>
-
+      <Weather />
     </>
   )
 }
--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -6,7 +6,14 @@ function Weather() {
         <section className="weather-section">
             <div className="container">
                 <div className="weather-wrapper">
+                    <div className="weather-card">
+                        <div className="weather-card_header">
+                            <h1>hI</h1>
+                        </div>
+                        <div className="weather-card_hero">
 
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
