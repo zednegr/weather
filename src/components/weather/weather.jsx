0b53f776221f943325f210a0331e95a961949c32
zednegr <negrzed@gmail.com>
2023-08-09 11:36:29 +0500

    update

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -1,4 +1,8 @@
 import "./weather.scss"
+import WeatherSun from "../../assets/svg/weather-sun.svg"
+import WeatherCloud from "../../assets/svg/weather-cloud.svg"
+import WeatherWint from "../../assets/svg/weather-wint.svg"
+import WeatherSunIcon from "../../assets/svg/weather-sun-icon.svg"
 
 function Weather() {
 
@@ -7,11 +11,31 @@ function Weather() {
             <div className="container">
                 <div className="weather-wrapper">
                     <div className="weather-card">
-                        <div className="weather-card_header">
-                            
-                        </div>
-                        <div className="weather-card_hero">
 
+                        <div className="weather-card_header">
+                            <div className="weather-sun">
+                                <img className="weather-sun-img" src={WeatherSun} alt="Sun" />
+                            </div>
+                            <div className="weather-c">
+                                <h2 className="weather-c_h2">35°</h2>
+                                <h4 className="weather-c_h4">Sunny</h4>
+                            </div>
+                        </div>
+
+                        <div className="weather-card_hero">
+                            <div className="weather-card_wint">
+                                <img src={WeatherWint} alt="Icon" />
+                                <h4 className="weather-card_wint_h4">16 km/h</h4>
+                            </div>
+                            <div className="weather-card_cloud">
+                                <img src={WeatherCloud} alt="Icon" />
+                                <h4 className="weather-card_cloud_h4">83 %</h4>
+                            </div>
+                            <div className="weather-card_Isun">
+                                <img src={WeatherSunIcon} alt="Icon" />
+                                <h4 className="weather-card_Isun_h4">2 of 10</h4>
+                            </div>
+                          
                         </div>
                     </div>
                 </div>
