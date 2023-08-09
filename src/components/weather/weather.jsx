e0fdeafc34c9dbc2eeed085003ff0152cb57e6c6
zednegr <negrzed@gmail.com>
2023-08-09 10:53:22 +0500

    update

---
+++ src/components/weather/weather.jsx
@@ -0,0 +1,16 @@
+import "./weather.scss"
+
+function Weather() {
+
+    return (
+        <section className="weather-section">
+            <div className="container">
+                <div className="weather-wrapper">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Weather
